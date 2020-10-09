258685bd050481ac89143204ad76eefeebc3ba5e
William Donovan-Seid <wdstwo@live.com>
2020-10-09 10:43:59 -0700

    add Views/Clients/Detail.cshtml with corresponding controller route

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -36,5 +36,12 @@ namespace HairSalon.Controllers
       return RedirectToAction("Index");
     }
 
+    public ActionResult Details(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
+    }
+
+
   }
 }
